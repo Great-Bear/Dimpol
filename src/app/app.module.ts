7f24ee3b15bb40215e3df79bae0be48ce9f976ce
Bogdan <bgroholsky@gmail.com>
2022-01-22 22:21:37 +0200

    regform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,35 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { Page404Component } from './components/page404/page404.component';
+import { AboutComponent } from './components/about/about.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { AuthorizationComponent } from './components/authorization/authorization.component';
+
+const appRoutes: Routes =[
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'registration', component: RegistrationComponent},
+  { path: 'authorization', component: AuthorizationComponent},
+  { path: '**', component: Page404Component }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Page404Component,
+    AboutComponent,
+    RegistrationComponent,
+    AuthorizationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
